83ce614dc9682061f5aee368126dcce4a814d9c8
Gustavo Lemos <81194465+GustaJLemos@users.noreply.github.com>
2022-01-11 22:58:21 -0300

    terminando de implementar o layout

--- src/App.js
+++
@@ -1,7 +0,0 @@
-import "./App.css";
-
-function App() {
-  return <div>alou ta funcionando?</div>;
-}
-
-export default App;
---
+++ src/pages/Index.jsx
@@ -0,0 +1,50 @@
+import classes from "./index.module.css";
+import { Link } from "react-router-dom";
+export default function Index() {
+  return (
+    <div id="home" className={classes.content}>
+      <div className={classes.container}>
+        <h2 className={classes.title}>Faça seu login</h2>
+        <div className={classes.separator} />
+        <form action="#">
+          {/* input e label do email */}
+          <label htmlFor="email" className={classes["sr-only"]}>
+            Email do usuário
+          </label>
+          <input
+            type="text"
+            name="email"
+            className={classes.input + " " + classes.email}
+            placeholder="Email"
+          />
+          {/* input e label do password */}
+          <label htmlFor="password" className="sr-only" />
+          <input
+            type="password"
+            name="password"
+            className={classes.input + " " + classes.password}
+            placeholder="Senha"
+          />
+        </form>
+        <div className={classes.separator} />
+        <Link to="/home" className={classes.button}>
+          Entrar
+          <img
+            src="/images/enter-room.svg"
+            alt="Entrar"
+            className={classes["button-img"]}
+          />
+        </Link>
+        <div className={classes.questions}>
+          <a href="#" className={classes["text-purple"]}>
+            esqueci minha senha!
+          </a>
+          <br />
+          <a href="#" className={classes["text-purple"]}>
+            está com problema para logar?
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
